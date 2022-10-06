33fab9ed58415bf588ad9d7f1142ed46b43a1084
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-10-05 18:15:32 -0600

    completed all problems

--- arrayMethods.js
+++ arrayMethods.js
@@ -40,9 +40,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map(num => num * 1.07 )
 
-
+console.log(postTaxPrices)
 
 ////////// PROBLEM 3 //////////
 
@@ -58,7 +58,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce((acc, curr) => acc + curr)
+
+console.log(totalPopulation)
 
 
 
@@ -83,14 +85,17 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-// const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(monster => monster.CP > 200)
 
-// jared's solution:
-// function callback(monster){
-//   return monster.CP > 200
-// }
-// const myStrongest = monstersInYourPocket.filter(callback)
+console.log(myStrongest)
+
+// THIS WORKS TOO:
+// const myStrongest = monstersInYourPocket.filter(function(elem) {
+//   return elem.CP > 200
+// })
+
 // console.log(myStrongest)
+
 
 ////////// PROBLEM 5 //////////
 
@@ -106,7 +111,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
+let orderTotals = orders.map(order => order.price + (order.price * order.tax))
 
+console.log(orderTotals)
 
 
 ////////// PROBLEM 6 //////////
@@ -126,3 +133,11 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const bobsTotal = purchases
+    .filter(bob => bob.owner === "Bob")
+    .reduce((a,c) => a + c.price, 0)
+
+console.log(bobsTotal)
+
+
+
